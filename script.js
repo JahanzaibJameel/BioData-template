16652d14553a234e5a3fb573a33c8a85ce5560e0
Muhammad Jahanzaib <m.jahanzaibjameel@gmail.com>
2025-04-13 18:33:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const toggleSwitch = document.getElementById("theme-switch");
+
+toggleSwitch.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark");
+    toggleSwitch.checked = true;
+  }
+});
